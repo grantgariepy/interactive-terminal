6adb5a3652af07039178c0cec75b531c79176cc6
grantgariepy <grantgariepy@protonmail.com>
2022-05-29 16:45:20 -0600

    commit

---
+++ js/commands.js
@@ -0,0 +1,88 @@
+
+var linkedin = "https://www.linkedin.com/in/grantgariepy/";
+
+var github = "https://github.com/grantgariepy/";
+var email = 'mailto:grant.gariepy@gmail.com';
+var resume = "https://grantgariepy.com/";
+var arx = "https://arxoccultus.io/";
+var pizza = "https://ggfoodshop.com/";
+var charts = "https://chartsbygrant.xyz/";
+var art = "https://dontblend.in/";
+
+whois = [
+  "<br>",
+  "Hey, I'm Grant!",
+  "I'm a full stack web developer and UI/UX Designer, who builds engaging websites like this one!",
+  
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "I don't know, who are you? Enter the command 'Email' and let me know who you are.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/grantgariepy' + "</a>",
+  
+  'github         <a href="' + github + '" target="_blank">github/grantgariepy' + "</a>",
+  "<br>"
+];
+
+
+projects = [
+  "<br>",
+  'static portfolio    <a href="' + resume + '" target="_blank">grantgariepy.com' + "</a>",
+  'web3 password manager    <a href="' + arx + '" target="_blank">arxoccultus.io' + "</a>",
+  'online food shop    <a href="' + pizza + '" target="_blank">ggfoodshop.com' + "</a>",
+  'mysql/php charts    <a href="' + charts + '" target="_blank">chartsbygrant.xyz' + "</a>",
+  'neuralnet art    <a href="' + art + '" target="_blank">dontblend.in' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Grant?',
+  '<span class="command">whoami</span>         Who are you?',
+  
+  '<span class="command">social</span>         Display social networks',
+  
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Grant Gariepy | Not A Corporation. All rights reserved.</span>',
+  "<span class='planet'>                                                      ___</span>",
+  "<span class='planet'>                                                  ,o88888</span>",
+  "<span class='planet'>                                               ,o8888888'</span>",
+  "<span class='planet'>                        ,:o:o:oooo.        ,8O88Pd8888'</span>",
+  "<span class='planet'>                     ,.::.::o:ooooOoOoO. ,oO8O8Pd888''</span>",
+  "<span class='planet'>                   ,.:.::o:ooOoOoOO8O8OOo.8OOPd8O8O'</span>",
+  "<span class='planet'>                  , ..:.::o:ooOoOOOO8OOOOo.FdO8O8'</span>",
+  "<span class='planet'>                 , ..:.::o:ooOoOO8O888O8O,COCOO'</span>",
+  "<span class='planet'>                , . ..:.::o:ooOoOOOO8OOOOCOCO'</span>",
+  "<span class='planet'>                 . ..:.::o:ooOoOoOO8O8OCCCC'o</span>",
+  "<span class='planet'>                    . ..:.::o:ooooOoCoCCC'o:o</span>",
+  "<span class='planet'>                    . ..:.::o:o:,cooooCo'oo:o:   ________                         __      ________                .__                        </span>",
+  "<span class='planet'>                 `   . . ..:.:cocoooo''o:o:::'  /  _____/_______ _____     ____ _/  |_   /  _____/ _____  _______ |__|  ____  ______  ___.__.</span>",
+  "<span class='planet'>                 .`   . ..::ccccoc''o:o:o:::'  /   \\  ___\\_  __ \\__   \\   /    \\\\   __\\ /   \\  ___ \\__  \\ \\_  __ \\|  |_/ __ \\ \\____ \\<   |  |</span>",
+  "<span class='planet'>                :.:.    ,c:cccc'':.:.:.:.:.'   \\    \\_\\  \\|  | \\/ / __ \\_|   |  \\|  |   \\    \\_\\  \\ / __ \\_|  | \\/|  |\\  ___/ |  |_> >\\___  |</span>",
+  "<span class='planet'>              ..:.:''`::::c:''..:.:.:.:.:.'     \\______  /|__|   (____  /|___|  /|__|    \\______  /(____  /|__|   |__| \\___  >|   __/ / ____|</span>",
+  "<span class='planet'>            ...:.'.:.::::''    . . . . .'              \\/             \\/      \\/                \\/      \\/                 \\/ |__|    \\/     </span>",
+  "<span class='planet'>           .. . ....:.'' `   .  . . ''</span>",
+  "<span class='planet'>         . . . ....''</span>",
+  "<span class='planet'>         .. . .''     © 2022</span>",
+  "<span class='planet'>        .</span>",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+
